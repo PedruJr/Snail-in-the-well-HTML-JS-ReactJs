6ca300295af708aab974020af2590c85d674e27f
Pedro Junior <you@example.com>
2021-04-26 11:00:08 -0300

    Project Finish

--- src/components/WormHole.js
+++ src/components/WormHole.js
@@ -1,69 +1,133 @@
-import React, { useState } from 'react'
-import worm from '../assets/worm.png';
-import './wormHole.css';
+import React, { useState } from "react";
+import worm from "../assets/worm.png";
+import "./wormHole.css";
 
 export default function WormHole() {
+  const [hole, setHole] = useState(20);
+  const [climb, setClimb] = useState(5);
+  const [fall, setFall] = useState(2);
+  const [stop, setStop] = useState(1);
+  const [totalClimb, setTotalClimb] = useState(0);
+  const [index, setIndex] = useState(0);
+  const [css, setCss] = useState("noColor");
+  const [text, setText] = useState("Inicie o percurso!");
 
-    const[hole, setHole] = useState('20');
-    const[climb, setClimb] = useState('5');
-    const[fall, setFall] = useState('2');
-    const[stop, setStop] = useState('1');
-    const[totalClimb, setTotalClimb] = useState('');
-    const[index, setIndex] = useState('0');
-    const[css, setCss] = useState('none');
-    
-        const number = 2;
-            const data = {
-            hole,
-            climb,
-            fall,
-            stop,
-            totalClimb,
-            index,
-            css
-        };
-    
-     function startClimbing  (e,data) =>{    
-        event.preventDefault();
+  const data = {
+    hole,
+    climb,
+    fall,
+    stop,
+    totalClimb,
+    index,
+    css,
+  };
 
+  function cssYellow() {
+    setCss("yellow");
+  }
 
-        var half = data.hole/2;
-        console.log(number);
-        
-        for(var i; i < 10; i++) {
-            var calc;
-            calc++;
-            setTotalClimb(calc);
-            console.log(totalClimb);
+  function cssGreen() {
+    setCss("green");
+  }
+
+  function startClimbing(event) {
+    event.preventDefault();
+
+    var half = data.hole / 2;
+    let total = 0;
+    let i = 0;
+    let oneSecond = 0;
+
+    setText("A minhoca iniciou sua jornada!");
+    setCss("none");
+
+    const sleep = (ms) => {
+      return new Promise((resolve) => setTimeout(resolve, ms));
+    };
+
+    function oneSec() {
+      return sleep(1000).then(console.log("one second..."));
+    }
+
+    console.log(data);
+
+    const forLoop = async () => {
+      for (; total <= data.hole; ) {
+        oneSecond = await oneSec();
+
+        total = total + data.climb - data.fall;
+        console.log(total);
+        i++;
+
+        if (total <= half) {
+            setText("A minhoca subiu: "+i+" vezes")
         }
-    }
+        if (total >= half) {
+          cssYellow();
+          setText(
+            "A minhoca esta na metade do buraco! subindo um total de: " +
+              i +
+              " vezes"
+          );
+          console.log(data.css);
+        }
+        if (total > data.hole) {
+          cssGreen();
+          setText(
+            "A minhoca saiu do buraco! subindo um total de: " + i + " vezes"
+          );
+          console.log("green works");
+        }
+      }
+    };
 
-  
-        return(
-            <div>
-                <header className="App-header">
-                    Fuga da minhoca
-                </header>
+    forLoop();
+  }
 
-                <div className="content">
-                    <section className="inputs">
-                    <form>
-                        <span className="inputs-span">Defina os valores! </span>
-                        <input type="text" id="hole" placeholder="Definir comprimento do buraco..."></input>
-                        <input type="text" id="climb" placeholder="Definir altura ao subir..."></input>
-                        <input type="text" id="fall" placeholder="Definir a distancia ao cair..."></input>
-                        <button className="button" type="submit">Definir</button>
-                    </form>
-                    </section>
+  function setValues(event) {
+    event.preventDefault();
+    console.log(data);
+  }
 
-                    <section className="worm-section">
-                    <div className="">
-                        <img src={worm} alt="Logo" />
-                    </div>
-                    <span className="worm-view"> Go minhoca!</span>
-                    <button className="button" type="submit" onClick={startClimbing}>Iniciar</button>
-                    </section>
-                </div>
-            </div>
-        )
-    }
+  return (
+    <div>
+      <header className="App-header">Escape do buraco</header>
+
+      <div className="content">
+        <section className="inputs">
+          <form onSubmit={setValues}>
+            <span className="inputs-span">Defina os valores em cm! </span>
+            <input
+              type="number"
+              onChange={(e) => setHole(parseInt(e.target.value))}
+              placeholder="Definir comprimento do buraco..."
+            ></input>
+            <input
+              type="number"
+              onChange={(e) => setClimb(parseInt(e.target.value))}
+              placeholder="Definir altura ao subir..."
+            ></input>
+            <input
+              type="number"
+              onChange={(e) => setFall(parseInt(e.target.value))}
+              placeholder="Definir a distancia ao cair..."
+            ></input>
+            <button className="button" type="submit">
+              Definir
+            </button>
+          </form>
+        </section>
+
+        <section className="worm-section">
+          <div className={css}>
+            <img src={worm} alt="Logo" />
+          </div>
+          <span className="worm-view">{text}</span>
+          <button className="button" type="submit" onClick={startClimbing}>
+            Iniciar
+          </button>
+        </section>
+      </div>
+    </div>
+  );
+}
